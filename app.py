0b1a5374bf71e7f9ce486ffd6d8ddfdae6f0de78
Carlos Giraldo <carlos.giraldo@pentius.com>
2024-01-10 12:37:56 -0500

    created a new message function as a test demo

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
-x = 20
+# function to print the email variables to the log
+def new_message(to_email, from_email, subject, message):
+    print("FROM: ", from_email)
+    print("TO: ", to_email)
+    print("Subject: ", subject)
+    print("Message: ", message)
+    return
 
-y = "Alejandra"
+
+new_message("receiver@whatever.com",
+            "me@here.com",
+            "Hey",
+            "Did you see the old lady fall on the sidewalk? Casi se mata")
+
+print("\n")
+
+new_message("alejandra@receive_email.com",
+            "carlos@sender_email.com",
+            "Yo",
+            "What's up?")
